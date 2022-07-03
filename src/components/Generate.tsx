e438c7cf4d9b701421570af1381c2371d140b0bd
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2022-07-03 00:09:39 -0300

    Update Generate.tsx

--- src/components/Generate.tsx
+++ src/components/Generate.tsx
@@ -30,11 +30,11 @@ return(
     thisPoke.style.display= 'none'
     setNumberPoke(thisPoke.getAttribute('src')?.replace('../src/assets/img/pokemon/','').replace('.png',''))
     let imgNumber = thisPoke.getAttribute('src')?.replace('../src/assets/img/pokemon/','').replace('.png','')
-      setPokemonImg(`../src/assets/img/pokemon_catch/pokemon (${imgNumber}).gif`)
+      setPokemonImg(`../assets/img/pokemon_catch/pokemon (${imgNumber}).gif`)
 
       return navigate('/catch')
   
-    }}> <img id={`img${index}`} src={`../src/assets/img/pokemon/${utils.random(4,140)}.png`} alt="" className="w-[36px] h-[36px] "/>
+    }}> <img id={`img${index}`} src={`../assets/img/pokemon/${utils.random(4,140)}.png`} alt="" className="w-[36px] h-[36px] "/>
    </a>
  </div>
  
